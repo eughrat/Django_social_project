0386a9b4bbc85a4dd8d10d7e696e69bb6d3db958
eughrat <michal.piernicki@gmail.com>
2022-02-17 15:56:29 +0100

    made join and leave views.py

--- groups/urls.py
+++ groups/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',views.ListGroups.as_view(),name='all'),
     path('new/',views.CreateGroup.as_view(name='create')),
     path('posts/in/<slug>/',views.SingleGroup.as_view(),name='single'),
-
+    path('join/in/<slug>/',views.JoinGroup.as_view(),name='join'),
+    path('join/in/<slug>/',views.LeaveGroup.as_view(),name='leave'),
 ]
